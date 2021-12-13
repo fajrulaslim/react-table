424b69a531ad055242175efa213bcab74f681cd3
Fajrul Aslim <30421357+fajrulaslim@users.noreply.github.com>
2021-12-13 15:23:49 +0700

    tutorial react table

---
+++ src/components/BasicTable.js
@@ -0,0 +1,79 @@
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS, GROUPED_COLUMNS } from './columns'
+import './table.css'
+
+export const BasicTable = () => {
+    // const columns = useMemo(() => COLUMNS, [])
+    const columns = useMemo(() => COLUMNS, [])
+    const data = useMemo(() => MOCK_DATA, [])
+
+    const tableInstance = useTable({
+        columns,
+        data
+    })
+
+    const { 
+        getTableProps,
+         getTableBodyProps, 
+         headerGroups, 
+         footerGroups,
+         rows, 
+         prepareRow 
+        } = tableInstance
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map((headerGroup) => {
+                        return (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {
+                                    headerGroup.headers.map((column) => {
+                                        return <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                    })
+                                }
+                            </tr>
+                        )
+                    })
+                }
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {
+                    rows.map((row) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps}>
+                                {
+                                    row.cells.map((cell) => {
+                                        return <td {...cell.getCellProps}>{cell.render('Cell')}</td>
+                                    })
+                                }
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+            <tfoot>
+                {
+                    footerGroups.map((footerGroup) => {
+                        return (
+                            <tr {...footerGroup.getFooterGroupProps()}>
+                                {
+                                    footerGroup.headers.map((column) => {
+                                        return <td {...column.getFooterGroupProps}>{column.render('Footer')}</td>
+                                    })
+                                }
+                            </tr>
+                        )
+                        
+                    })
+                }
+            </tfoot>
+        </table>
+        
+        
+    )
+}
---
+++ src/components/GlobalFilter.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+// export const GlobalFilter = ({ filter, setFilter}) => {
+//     return (
+//         <span>
+//             Search: { ' '}
+//             <input value={filter || ''} onChange={e => setFilter(e.target.value)} />
+//         </span>
+//     )
+// }
+
+import React, { useState } from 'react'
+import { useAsyncDebounce } from 'react-table'
+
+export const GlobalFilter = ({ filter, setFilter}) => {
+    const [value, setValue] = useState(filter)
+
+    const onChange = useAsyncDebounce( value => {
+        setFilter(value || undefined)
+    }, 1000)
+
+    return (
+        <span>
+            Search: { ' '}
+            <input value={value || ''} 
+            onChange={ (e) => {
+                setValue(e.target.value)
+                onChange(e.target.value)
+            }} />
+        </span>
+    )
+}
